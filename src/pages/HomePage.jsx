a6155c96f354628d07d290a81a080658320cbb0e
Hariksh <hariksh2007@gmail.com>
2025-04-21 23:29:02 +0530

    hey

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,7 +15,8 @@ const HomePage = ({ onStart, onTakeQuiz }) => {
     <div className="home-page">
       <h1>Welcome to What Should I Do Now?</h1>
       <p style={{ fontStyle: "italic", marginBottom: "20px" }}>{randomQuote}</p>
-      <button className="start-button" onClick={onStart}>
+      <button className="start-button" onClick={onStart}
+      style={{ marginRight: "20px" }}>
         Start Selection
       </button>
       <button className="quiz-button" onClick={onTakeQuiz}>
